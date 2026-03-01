d50277d246cf37d38e19e57e7cc4297ff6d9cde0
Jsaur <6awesomejoe@gmail.com>
2026-03-01 03:58:32 -0500

    update tempdata to save pics as jpg

--- src/scripts/camera_ui.gd
+++ src/scripts/camera_ui.gd
@@ -8,17 +8,18 @@ var second_pic
 const next_scene = preload("uid://y33a508r54up")
 
 func _ready() -> void:
-	textlabel.text = "[center]Please take a picture of just the pole's tag."
+	textlabel.text = "[center]Please take a picture of just the pole's tag (plate)."
 
 func _on_camera_button_pressed() -> void:
 	var img = subviewport.get_texture().get_image()
-	img.save_png("res://data/temp.png")
 	pic_count += 1
 	if pic_count == 1:
 		first_pic = img
+		img.save_jpg("res://tempdata/plate.jpg")
 		textlabel.text = "[center]Now take a picture of the whole pole."
 	elif pic_count == 2:
 		second_pic = img
+		img.save_jpg("res://tempdata/pole.jpg")
 		GlobalData.pole_data = PoleData.create_from_pics([first_pic, second_pic])
 		get_tree().root.add_child(next_scene.instantiate())
 		queue_free()
